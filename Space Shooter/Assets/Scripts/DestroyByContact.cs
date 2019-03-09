a5e68fb2e830e738753698d6ae7d7130a6ad5ebe
I Kadek Yuda Budipratama Giri <13516115@std.stei.itb.ac.id>
2019-03-09 23:25:45 +0700

    Finished 4.1 Extending The Game, Scrolling Background, Enemies, etc.

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    private GameController gameController;
+    public int scoreValue;
+
+    private void Start()
+    {
+        GameObject gameObjectController = GameObject.FindWithTag("GameController");
+        if (gameObjectController != null)
+        {
+            gameController = gameObjectController.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary" || other.tag == "Enemy")
+        {
+            return;
+        }
+
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+        gameController.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
